15f4120cce55dfb4512e7d37ebaf00133a066001
Vedran Stojnović <phidrho@gmail.com>
2023-04-25 18:41:16 +0200

    dodan drugi primjer za komponentu iz klase

--- src/components/KorisnikKlasa.jsx
+++ src/components/KorisnikKlasa.jsx
@@ -10,4 +10,12 @@ export default class KorisnikKlasa extends React.Component {
           <p>Moje ime je {ime} i imam {godine} godina.</p>
         </div>);
     }
+    // DRUGA VARIJANTA: (bez mapiranja varijabli)
+    // render(){
+    //   return(
+    //     <div>
+    //       <h6>KorisnikKlasa komponenta:</h6>
+    //       <p>Moje ime je {this.props.ime} i imam {this.props.godine} godina.</p>
+    //     </div>);
+    // }
 }
