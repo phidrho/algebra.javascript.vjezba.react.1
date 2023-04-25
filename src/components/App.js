5a3234b5f4468dd20de6a58c42564450e0eff2c3
Vedran Stojnović <phidrho@gmail.com>
2023-04-25 17:17:42 +0200

    App funkcija pretvorena u klasu

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,9 @@ import {GlavnaKomponenta} from './GlavnaKomponenta';
 import WelcomeFunkcija from './WelcomeFunkcija';
 import WelcomeKlasa from './WelcomeKlasa';
 
+//vjezba PROPS 5.8
+import React from "react";
+
 var zbrojIUmnozak = sum(num1, num2) * pi;
 
 //vjezba JSX - 5.5.
@@ -27,55 +30,61 @@ var mojString2 = "'MOJ STRING IZ VARIJABLE 2'";
 //vjezba JSX - 5.7
 
 
-function App() {
-  var rndstr = randomstring.generate();
-  console.log("Random string" + randomstring.generate());
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>{mojaVarijabla}</h1>
-        <h2>Učitano ime osobe je: {Person.name} i ima {Person.godine} godina.</h2>
-        <p>Zbroj {num1} i {num2} i umnožak sa PI je {zbrojIUmnozak}</p>
-        <p>Razlika {num2} i {num1} je {oduzimanje(num1, num2)}</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Random string varijabla: {rndstr}</p>
-        <p>Random string direktno pozvan: {randomstring.generate({
-                                                length: 12,
-                                                charset: 'alphabetic'
-                                              })
-                                          }</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+//vjezba PROPS 5.8 - pretvaramo u App klasu
+class App extends React.Component {
 
-        <h1>Učitane komponente iz JSX-a - 5.5</h1>
-        <h2>Komponenta 1:</h2>
-        <Komponenta1 />
 
-        <h2>Komponenta 2:</h2>
-        <Komponenta2 podatak={mojString1} />
+  render() {
+    var rndstr = randomstring.generate();
+    console.log("Random string" + randomstring.generate());
 
-        <h2>Komponenta 3:</h2>
-        <Komponenta3 podatak1={mojString1} podatak2={mojString2} />
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>{mojaVarijabla}</h1>
+          <h2>Učitano ime osobe je: {Person.name} i ima {Person.godine} godina.</h2>
+          <p>Zbroj {num1} i {num2} i umnožak sa PI je {zbrojIUmnozak}</p>
+          <p>Razlika {num2} i {num1} je {oduzimanje(num1, num2)}</p>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>Random string varijabla: {rndstr}</p>
+          <p>Random string direktno pozvan: {randomstring.generate({
+                                                  length: 12,
+                                                  charset: 'alphabetic'
+                                                })
+                                            }</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
 
-        <h1>Učitane komponente iz JSX-a - 5.6</h1>
-        <GlavnaKomponenta />
+          <h1>Učitane komponente iz JSX-a - 5.5</h1>
+          <h2>Komponenta 1:</h2>
+          <Komponenta1 />
 
-        <h1>Učitane komponente iz JSX-a - 5.7</h1>
-        <WelcomeFunkcija />
-        <WelcomeKlasa />
+          <h2>Komponenta 2:</h2>
+          <Komponenta2 podatak={mojString1} />
 
-      </header>
-    </div>
-  );
+          <h2>Komponenta 3:</h2>
+          <Komponenta3 podatak1={mojString1} podatak2={mojString2} />
+
+          <h1>Učitane komponente iz JSX-a - 5.6</h1>
+          <GlavnaKomponenta />
+
+          <h1>Učitane komponente iz JSX-a - 5.7</h1>
+          <WelcomeFunkcija />
+          <WelcomeKlasa />
+
+        </header>
+      </div>
+    );
+                                        }
 }
 
 export default App;
