9f25306b61a87daccb9fbfef22c09f439e084d11
Vedran Stojnović <phidrho@gmail.com>
2023-05-15 17:18:17 +0200

    priprema za API poziv

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 // import App from './components/App';
 // import AppContext from './components/AppContext';
 // import AppHOC from './components/AppHOC';
-import AppRouter from './components/AppRouter';
+//import AppRouter from './components/AppRouter';
+import AppAPI from './components/AppAPI';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     // <App />
     // <AppContext />
     // <AppHOC />
-    <AppRouter />
+    // <AppRouter />
+    <AppAPI />
 );
