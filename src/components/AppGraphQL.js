b10f85fae7ea45721427764f6a0e255f11bac025
Vedran Stojnović <phidrho@gmail.com>
2023-05-15 18:37:22 +0200

    korigiran kod, deinstalirani apollo-cache-inmemory apollo-client

--- src/components/AppGraphQL.js
+++ src/components/AppGraphQL.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider, useQuery } from '@apollo/react-hooks';
+// import ApolloClient from 'apollo-boost';
+import { ApolloProvider, useQuery, InMemoryCache, ApolloClient } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
+
+//importamo dodane biblioteke
+import { createHttpLink } from 'apollo-link-http';
+
 
 // LINK ZA DEFINICIJU KLIJENTA KOJI PRIMA GRAPHQL UPITE:
 // https://48p1r2roz4.sse.codesandbox.io
@@ -16,11 +20,20 @@ import gql from 'graphql-tag';
 // }
 
 
-
-// kreiramo naš klijent za GraphQL - definiramo gdje je resurs za podatke
-const client = new ApolloClient({
+const httpLink = new createHttpLink({
     uri: 'https://48p1r2roz4.sse.codesandbox.io',
 });
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+});
+
+
+// // kreiramo naš klijent za GraphQL - definiramo gdje je resurs za podatke
+// const client = new ApolloClient({
+//     uri: 'https://48p1r2roz4.sse.codesandbox.io',
+// });
 
 // naša komponenta
 function ExchangeRates() {
