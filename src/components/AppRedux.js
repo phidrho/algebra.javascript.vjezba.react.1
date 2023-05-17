b47e9ca0634244ad01b0dd95d80ff2e482d072f8
Vedran Stojnović <phidrho@gmail.com>
2023-05-17 18:01:17 +0200

    appredux priprema gotova

--- src/components/AppRedux.js
+++ src/components/AppRedux.js
@@ -1,9 +1,38 @@
 // prvo potrebno instalirati npm install redux react-redux
 import React from "react";
+import { connect } from "react-redux";
 
 
-function AppRedux(){
-
+function AppRedux({ brojac, dodajJedan, oduzmiJedan, resetiraj }) {
+    return (
+        <div>
+            <p>
+                <span>
+                    <button onClick={oduzmiJedan}> - </button>
+                    <span className="counter">Broj: {brojac}</span>
+                    <button onClick={dodajJedan}> + </button>
+                </span>
+            </p>
+            <p>
+                <button onClick={resetiraj}>Resetiraj broj</button>
+            </p>
+        </div>
+    );
 }
 
-export default AppRedux;
+function mapStateToProps(state) {
+    return {
+        brojac: state,
+    }
+}
+
+const mapDispatchToProps = {
+    dodajJedan: () => dodajNaBrojac(1),
+    oduzmiJedan: () => oduzmiSaBrojaca(1),
+    resetiraj: () => setirajBrojac(0)
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(AppRedux);
